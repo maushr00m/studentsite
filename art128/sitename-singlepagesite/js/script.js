c6f20d4977d37c2ddcd22bcf0abeb2984728f0f0
maushr00m <had4@hawaii.edu>
2025-03-20 20:29:53 -1000

    single page site

---
+++ art128/sitename-singlepagesite/js/script.js
@@ -0,0 +1,23 @@
+document.addEventListener('DOMContentLoaded', function () {
+
+  //TOGGLE MENU NAV VIA ADDING/REMOVING A DATA-ATTRIBUTE
+  const menubutton = document.querySelector('.menu-button');
+  const menunav = document.querySelector('.toggle-nav');
+  menubutton.addEventListener('click', function () {
+      if (menunav.getAttribute('data-navstate') === 'open') {
+          menunav.setAttribute('data-navstate', 'closed');
+      } else {
+          menunav.setAttribute('data-navstate', 'open');
+      }
+  });
+
+  //TOGGLE ACCORDIAN VIA ADDING/REMOVING A CLASS
+  var acc = document.querySelectorAll('.accordian');
+  var i;
+  for (i = 0; i < acc.length; i++) {
+      acc[i].getElementsByTagName('h4')[0].onclick = function () {
+          this.parentElement.classList.toggle("open");
+      }
+  }
+
+})
